010d8764300a8d7e1519def158ed6c212d5dc614
Nematullo <nematullo@MacBook-Pro.local>
2023-06-24 12:23:46 +0500

    first

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,9 +1,93 @@
-import React from 'react'
+import React from 'react';
+import logo_foter from '../../assets/images/lg.png'
+import wk_foter from '../../assets/images/vk.png'
+import fb_foter from '../../assets/images/fb.png'
+import link_foter from '../../assets/images/linkedin.png'
+import line from '../../assets/images/Line 6.png'
+import vektor from '../../assets/images/Vector(1).png'
+
+import './footer.scss'
 
 const Footer = () => {
-    return <footer className='container'>
-        {/* Footer */}
-        </footer>;
-}
+    const top = () => {
+        window.scrollTo(0, 0)
+    }
+    return (
+        <section className='S__All'>
+            <footer className='container'>
+                <div className='footer_logo'>
+                    <img src={logo_foter} alt="" />
+                    <div className='lg_wk'>
+                        <img src={wk_foter} alt="" />
+                        <img src={fb_foter} alt="" />
+                        <img src={link_foter} alt="" />
+                    </div>
+                </div>
+                <img className='line' src={line} alt="" />
 
-export default Footer
+                <div className='Yon-yon'>
+                    <div className='yong'>
+                        <div>
+                            <h2>Продукция</h2><br />
+                            <p>Ламинатные тубы</p>
+                            <p>Экструзионные тубы</p>
+                            <p>Другая упаковка</p>
+                        </div>
+
+                        <div>
+                            <h2>Компания</h2><br />
+                            <p>О нас</p>
+                            <p>Наша команда</p>
+                            <p>Сертификаты</p>
+                        </div>
+
+                        <div>
+                            <h2>Разделы</h2><br />
+                            <p>Контакты</p>
+                            <p>Новости</p>
+                            <p>Вакансии</p>
+                        </div>
+                    </div>
+                    <div className='y'>
+                        <div className='yonga'>
+                            <div>
+                                <h3>Беларусь</h3><br />
+                                <p>+375 (17) 270 53 77</p>
+                                <p>+375 (17) 270 53 78</p>
+                            </div>
+
+                            <div>
+                                <h3>Европа</h3><br />
+                                <p>+48 73 1111 044</p>
+                            </div>
+                        </div><br /><br />
+                        <div className='yonga'>
+                            <div>
+                                <h3>Москва</h3><br />
+                                <p>+7 (495) 280 73 44</p>
+                                <p>+7 (495) 280 73 44</p>
+                            </div>
+
+                            <div>
+                                <h3>Екатеринбург</h3><br />
+                                <p>+7 (343) 346 82 06</p>
+                            </div>
+                        </div>
+                        <br /><br />
+                    </div>
+                    <span className='as'>© 2022 Leangroup. All Rights Reserved. <br /> Разработка и продвижение сайтов SkyWeb.by</span>
+
+                    <p className='back' onClick={top}>
+                        <img src={vektor} alt="" />
+                    </p>
+
+
+                </div>
+            </footer>
+        </section>
+    );
+};
+
+
+
+export default Footer;
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,14 +1,84 @@
-import React from 'react'
-import "./header.module.scss"
+import React, { Component } from 'react'
+import logo from '../../assets/images/logo.png'
+import line from '../../assets/images/Line 11.png'
+import './header.scss'
 
-import "../../main"
+export class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nav: false,
+    }
+  }
+  render() {
+    const openNavbar = () => {
+      this.setState({ nav: !this.state.nav })
+    }
+    return (
+      <div className='nav'>
+        <header className='a container'>
+          <div className='S__navv'>
+            <div>
+              <img src={logo} alt="" />
+            </div>
+            <ul>
+              <li className='item'>
+                <a href="https://www.instagram.com/samariddin_nurmamatov/">Продукция</a>
+                <ul className="dropdown">
+                  <li><a href="https://www.instagram.com/samariddin_nurmamatov/">Ламинатные тубы</a></li>
+                  <li><a href="https://www.instagram.com/samariddin_nurmamatov/">Экструзионные тубы</a></li>
+                  <li><a href="https://www.instagram.com/samariddin_nurmamatov/">Switzerla</a></li>
+                  <li><a href="https://www.instagram.com/samariddin_nurmamatov/">Japan</a></li><br />
+                </ul>
+              </li>
+              <img src={line} alt="" />
+              <li>Сертификаты</li>
+              <img src={line} alt="" />
+              <li>Наша команда</li>
+              <img src={line} alt="" />
+              <li>О нас</li>
+              <img src={line} alt="" />
+              <li>Новости</li>
+              <img src={line} alt="" />
+              <li>Вакансии</li>
+              <img src={line} alt="" />
+              <li>Контакты</li>
+            </ul>
+          </div>
+          <div className='bb'>
+            <p><span>RU</span> / EN</p>
+            <button onClick={openNavbar}><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuras_I78qJJti5cOq4YVKIHQCg_PjVEq-8Q&usqp=CAU" alt="" /></button>
+          </div>
 
-const Header = () => {
-  return (
-    <header>
 
-    </header>
-  )
+
+          {this.state.nav ?
+
+            <div className='S__navv_responcive'>
+              <ul>
+                <li className='item'>
+                  <a href="https://www.instagram.com/samariddin_nurmamatov/">Продукция</a>
+
+                </li>
+                --------
+                <li>Сертификаты</li>
+                --------
+                <li>Наша команда</li>
+                --------
+                <li>О нас</li>
+                --------
+                <li>Новости</li>
+                --------
+                <li>Вакансии</li>
+                --------
+                <li>Контакты</li>
+              </ul>
+            </div>
+            : ""}
+        </header>
+      </div>
+    )
+  }
 }
 
-export default Header
+export default Header;
